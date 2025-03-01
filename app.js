0b972decc43f2220b3b0feb982b440f9557f645e
AlanSAndovalLoZA <alansaulsandoval@gmail.com>
2025-02-28 19:53:17 -0600

    Terminado-Java app.js

--- app.js
+++ app.js
@@ -1,1 +1,40 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+function agregarAmigo(){
+    let inputNombre = document.getElementById('amigo'); // Obtener el input
+    let nombre = inputNombre.value.trim();
+    if(nombre){
+        amigos.push(nombre);
+        inputNombre.value ='';
+        inputNombre.focus();
+
+    }else{
+        alert('Ingresa un nombre, dejaste la cadena vacia');
+    }
+    imprimir(nombre);
+}
+
+function imprimir(nombre){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    amigos.forEach(nombre => {
+        let nuevoElemento = document.createElement('li'); // Crear un <li>
+        nuevoElemento.textContent = nombre; // Asignar el texto del nombre al <li>
+        lista.appendChild(nuevoElemento); // Agregar el <li> a la lista <ul>
+    });
+}
+
+function sortearAmigo(){
+    if(amigos.length>0){
+        let amigoSecreto = Math.floor(Math.random()*amigos.length);
+        let lista = document.getElementById("resultado");
+        lista[amigoSecreto];
+        lista.innerHTML = `El amigo secreto es: ${amigos[amigoSecreto]}`;
+        let limpiarLista = document.getElementById('listaAmigos');
+        limpiarLista.innerHTML='';
+    }else{
+        alert('No has ingresado ningun amigo!');
+
+    }
+    
+}
